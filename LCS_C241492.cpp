9417109848825b1a1e292643070f51fdcde6394d
Tahmina Sultana Reshmi <tahminasultanareshmin@gmail.com>
2025-10-08 19:01:23 +0600

    Add files via upload

---
+++ LCS_C241492.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string findLCS(string X, string Y) {
+    int m = X.length();
+    int n = Y.length();
+    int L[m + 1][n + 1];
+
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+            else if (X[i - 1] == Y[j - 1])
+                L[i][j] = L[i - 1][j - 1] + 1;
+            else
+                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+        }
+    }
+
+    int index = L[m][n];
+    string lcs(index, '\0');
+    int i = m, j = n;
+
+    while (i > 0 && j > 0) {
+        if (X[i - 1] == Y[j - 1]) {
+            lcs[index - 1] = X[i - 1];
+            i--; j--; index--;
+        } else if (L[i - 1][j] > L[i][j - 1])
+            i--;
+        else
+            j--;
+    }
+
+    return lcs;}
+
+int main() {
+    string X, Y;
+    cout << "Enter first string: ";
+    cin >> X;
+    cout << "Enter second string: ";
+    cin >> Y;
+    string lcs = findLCS(X, Y);
+    cout << "Length of LCS: " << lcs.length() << endl;
+    cout << "LCS: " << lcs << endl;
+
+    return 0;}
+
