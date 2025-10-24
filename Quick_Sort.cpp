41d4547397b66a55eb8d427359b8d7c612c4c2ff
Reshminn.. <tahminasultanareshmin@gmail.com>
2025-10-24 16:41:17 +0600

    Quick_Sort.cpp

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -1,43 +1,49 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;        } }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return (i + 1);}
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+    int pivot = arr[low];
+    int i = low, j  = high;
+    while(i<j){
+    while(arr[i]<=pivot)
+    {
+        i++; }
+    while(arr[j]>pivot)
+    {
+        j--; }
+    if(i<j)
+    {
+        swap(arr[i], arr[j]); }
     }
+    swap(arr[low], arr[j]);
+    return j;
 }
 
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+       int j = partition(arr, low, high);
+        quickSort(arr, low, j - 1);
+        quickSort(arr, j + 1, high);
+    }
+}
 int main() {
     int n;
     cout << "Enter number of elements: ";
     cin >> n;
 
     int arr[n];
-    cout << "Enter  elements: ";
+    cout << "Enter elements: ";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];    }
+        cin >> arr[i];
+    }
+
     quickSort(arr, 0, n - 1);
+
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";    }
+        cout << arr[i] << " ";
+    }
     cout << endl;
+
     return 0;
 }
-
-
