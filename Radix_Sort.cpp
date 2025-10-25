05f5602250702644b5ac402ec492972d7a9ddb5a
Reshminn.. <tahminasultanareshmin@gmail.com>
2025-10-25 19:49:12 +0600

    Radix_Sort.cpp

--- Radix_Sort.cpp
+++ Radix_Sort.cpp
@@ -1,57 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int getMax(vector<int>& arr) {
-    int maxVal = arr[0];
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i] > maxVal)
-            maxVal = arr[i];
+int getMax(int arr[], int n)
+{
+    int maxval = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= maxval)
+        {
+            maxval = arr[i];
+        }
     }
-    return maxVal;
-    }
-void countingSort(vector<int>& arr, int exp) {
-    int n = arr.size();
-    vector<int> output(n);
+    return maxval;
+}
+
+void countingsort(int arr[], int n, int pos)
+{
     int count[10] = {0};
-    for (int i = 0; i < n; i++) {
-        int digit = (arr[i] / exp) % 10;
-        count[digit]++;
+    int output[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        count[(arr[i] / pos) % 10]++;
     }
-    for (int i = 1; i < 10; i++) {
+
+    for (int i = 1; i < 10; i++)
+    {
         count[i] += count[i - 1];
     }
-    for (int i = n - 1; i >= 0; i--) {
-        int digit = (arr[i] / exp) % 10;
-        output[count[digit] - 1] = arr[i];
-        count[digit]--;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        output[--count[(arr[i] / pos) % 10]] = arr[i];
     }
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++)
+    {
         arr[i] = output[i];
     }
 }
 
-void radixSort(vector<int>& arr) {
-    int maxVal = getMax(arr);
+void radixsort(int arr[], int n)
+{
+    int m = getMax(arr, n);
+    for (int pos = 1; m / pos > 0; pos *= 10)
+    {
+        countingsort(arr, n, pos);
+    }
+}
 
-    for (int exp = 1; maxVal / exp > 0; exp *= 10) {
-        countingSort(arr, exp);
-    } }
-int main() {
+int main()
+{
     int n;
     cout << "Enter number of elements: ";
     cin >> n;
 
-    vector<int> arr(n);
-    cout << "Enter non-negative integers: ";
-    for (int i = 0; i < n; i++) {
+    int arr[n];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
-    radixSort(arr);
+    radixsort(arr, n);
 
     cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";}
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
     cout << endl;
+
     return 0;
 }
