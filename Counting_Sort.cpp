bca0be0027090a465d65a7cbcf61b737e83d905e
Reshminn.. <tahminasultanareshmin@gmail.com>
2025-10-25 15:56:52 +0600

    Counting_Sort.cpp

--- Counting_Sort.cpp
+++ Counting_Sort.cpp
@@ -1,39 +1,60 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void countingSort(int arr[], int n) {
+void countingsort(int arr[], int n)
+{
     int maxval = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > maxval)
+    for(int i = 1; i<n; i++)
+    {
+        if(arr[i]>=maxval)
+        {
             maxval = arr[i];
+        }
     }
-
-    int count[101] = {0};
-    int output[100];
-    for (int i = 0; i < n; i++)
+    int count[maxval+1] = {0};
+    int output[n];
+    for(int i = 0; i<maxval; i++)
+    {
         count[arr[i]]++;
-    for (int i = 1; i <= maxval; i++)
-        count[i] += count[i - 1];
-    for (int i = n - 1; i >= 0; i--) {
-        output[count[arr[i]] - 1] = arr[i];
+    }
+    for(int i = 1; i<=maxval; i++)
+    {
+        count[i] += count[i-1];
+    }
+    for(int i = n-1; i>=0; i--)
+    {
+        output[count[arr[i]]-1] = arr[i];
         count[arr[i]]--;
     }
-    for (int i = 0; i < n; i++)
-        arr[i] = output[i]; }
 
-int main() {
+    for(int i = 0; i<n; i++)
+    {
+        arr[i] = output[i];
+    }
+}
+
+int main()
+{
     int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-    int arr[100];
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    countingSort(arr, n);
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+    cout<< "Number of elements: ";
+    cin>>n;
+
+    int arr[];
+    cout<< "Enter elements: ";
+    for(int i = 0; i<n; i++)
+    {
+        cin>> arr[i];
+    }
+
+    countingsort(arr, n);
+
+    cout<< "Sorted array: ";
+    for(int i = 0; i<n; i++)
+    {
+        cout<< arr[i] << " ";
+    }
+        cout<<endl;
+
     return 0;
 }
 
